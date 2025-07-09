dfa70cd6a2fb05026144c7906464660ab691855a
James Barry <james.barry-2@student.manchester.ac.uk>
2025-07-09 19:08:20 +0100

    Added skeleton

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import random
 from game_map import GameMap
 from character import Character
@@ -19,25 +18,12 @@ while True:
         print("You did it. GAME OVER")
         break
 
-=======
-import random
-from game_map import GameMap
-from character import Character
-from utils import take_turn
-
-size = 3
-map = GameMap(height=size, width=size)
-guy = Character(name='Guy', level = 1, health=40, max_health=40, position=(0, 0), inventory=[], strength = 4, dexerity = 4, defence = 3, weapon='sword', attack_power=10)
-
-map.generate_map()
-map.print_map(guy.position[0], guy.position[1], map.world_map, False)
-
-
-
-while True:
-    take_turn(guy, map)
-    if guy.position == (size - 1, size - 1):
-        print("You did it. GAME OVER")
-        break
-
->>>>>>> 2b0734f8ce33ed860adc69e4354616b534041f9d
+# Need to do:
+# - Add a way to exit the game
+# - More combat options
+#   - New monster types
+#   - Spells
+# - New map shapes
+# - Arrow keys to move
+# - Add see whole map functionality to 'Old Map'
+# - Levels
